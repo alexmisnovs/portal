2ff34a93f54f1a3189fedfbf045c8c4f082c17f5
Alex MacBook air <aleksandrmisnov@gmail.com>
2020-02-23 22:17:43 +0000

    modified routes for UC fetching

--- portal/routes/web.php
+++ portal/routes/web.php
@@ -30,3 +30,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/orders', 'OrdersController@index')->name('orders');
 Route::post('/orders/get-uc-order', 'OrdersController@fetchOrderFromUC');
+
+Route::get('/orders/uc-api/{order_id?}', 'OrdersController@fetchUCOrder');
