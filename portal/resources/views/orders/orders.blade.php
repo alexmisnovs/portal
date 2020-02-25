f5d2452fa80974953e3dcf3dc71b595fd9d946e6
Alex MacBook air <aleksandrmisnov@gmail.com>
2020-02-25 21:11:26 +0000

    Working on fetch order function

--- portal/resources/views/orders/orders.blade.php
+++ portal/resources/views/orders/orders.blade.php
@@ -3,46 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Orders</div>
-
-                <div class="card-body">
-
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                        <form action="{{route('get-order')}}" method="POST">
-                            @csrf
-                            <input type="text" name="order_id" id="order_id">
-
-                            <input type="submit" value="Get Affiliate Details">
-
-                        </form>
-                        @error('api')
-                        <span>{{ $message }}</span>
-                        @enderror
-
-                        @if($data ?? '')
-                            Listing order details: </br>
-                            <?php
-//                                echo "<pre>";print_r($result)
-                            ?>
-                            Affiliate details:
-                            Network: {{ $data['order']['checkout']['custom_field7'] }} </br>
-                            Campaign: {{ $data['order']['checkout']['custom_field2'] }} </br>
-                            Affiliate Id: {{ $data['order']['checkout']['custom_field3'] }} </br>
-                            Click Id: {{ $data['order']['checkout']['custom_field4'] }}
-
-                        @else
-                        <div>Please enter order id above</div>
-                        @endif
-                </div>
-            </div>
-        </div>
+ 
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Generate Hold Order from UC</div>
